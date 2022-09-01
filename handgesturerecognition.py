630d9d63ab2707bd5825219b1180843162ea6604
FrancescoStoppa00 <102795646+FrancescoStoppa00@users.noreply.github.com>
2022-09-01 19:05:12 +0200

    nuovo dataset

--- handgesturerecognition.py
+++ handgesturerecognition.py
@@ -10,6 +10,7 @@ from tensorflow.python.keras.metrics import Precision, Recall, BinaryAccuracy
 #maxpooling2d serve per condensare tutti i dati e riportare quelli più importanti
 import math
 from cvzone.HandTrackingModule import HandDetector
+import time
 
 #righe necessarie per evitare errori causati da grandi dataset e dal consumo di risorse
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -24,29 +25,32 @@ data_dir = 'data123'
 #va a ridimensionare tutte le immagini del dataset
 data = tf.keras.utils.image_dataset_from_directory('data123', image_size = (400,400))
 
-#rendo il dataset un iteratore numpy in modo da poter 'viaggiare' nel dataset
-data_iterator = data.as_numpy_iterator()
-#il batch che è composto da due parti una prima rappresenta le immagine, nel nostro caso sono 32 poiche batch_size=32 per default
-#la seconda la label, quindi un array di 32 numeri dove ogni numero ci dice l'etichetta dell'immagine corrispondente
-batch = data_iterator.next() 
+# #rendo il dataset un iteratore numpy in modo da poter 'viaggiare' nel dataset
+# #il batch che è composto da due parti una prima rappresenta le immagine, nel nostro caso sono 32 poiche batch_size=32 per default
+# #la seconda la label, quindi un array di 32 numeri dove ogni numero ci dice l'etichetta dell'immagine corrispondente
 
-#PREPROCESSING DATA
-#fase in cui modifico le immagini in range che vanno da 0 a 1 ainvece che da 0 a 255 
-#ciò permette al mio modello di addestrarsi più velocemente e produrre risultati migliori
-data = data.map(lambda x,y: (x/255,y)) #x rappresenta le immagini, y le labels
-print(len(data))
-#DIVIDO IL MIO SET
+# #PREPROCESSING DATA
+# #fase in cui modifico le immagini in range che vanno da 0 a 1 ainvece che da 0 a 255 
+# #ciò permette al mio modello di addestrarsi più velocemente e produrre risultati migliori
+data = data.map(lambda x,y: (x/255.0,y)) #x rappresenta le immagini, y le labels
+print('batch1')
+scaled_iterator = data.as_numpy_iterator()
+batch = scaled_iterator.next()
+print(batch[0].max())
+print(batch[0].min()) 
+print(batch[0].shape)
+# #DIVIDO IL MIO SET
 train_size = int(len(data)*.7)
-val_size = int(len(data)*.2)+1
-test_size = int(len(data)*.1)+1
-print(train_size)
-print(val_size)
-print(test_size)
+val_size = int(len(data)*.2)
+test_size = int(len(data)*.1)
 
-#dico quanti batch sono necessari per il mio training, e dopo quanti batch dovrò utilizzzare gli altri data
+# #dico quanti batch sono necessari per il mio training, e dopo quanti batch dovrò utilizzzare gli altri data
 train = data.take(train_size)
 val = data.skip(train_size).take(val_size) 
 test = data.skip(train_size+val_size).take(test_size) 
+print(len(train))
+print(len(val))
+print(len(test))
 
 #BUILD DEEP LEARNING MODEL
 model = Sequential()
@@ -61,16 +65,15 @@ model.add(Flatten()) #appiattisco i dati, ovvero converto tutto in un array ad u
 model.add(Dense(256, activation='relu'))#256 sono i neuroni presenti in questo livello
 model.add(Dense(1, activation='sigmoid')) #singolo output che sarà o 0 o 1 grazie alla sigmoide
 #utilizzo come funzione di loss una classificazione binaria, e voglio tener d'occhio l'accuracy della nostra rete
-model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+model.compile('adam', loss='binary_crossentropy', metrics=[tf.keras.metrics.BinaryAccuracy()])
 print(model.summary())
 
 #TRAIN
 logdir = 'logs'
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir = logdir)
-hist = model.fit(train, epochs=20, validation_data=val, callbacks = [tensorboard_callback])
-# print(hist.history())
+hist = model.fit(train, epochs=20, validation_data=train, callbacks = [tensorboard_callback])
 
-#EVALUATE PERFORMANCE 
+# #EVALUATE PERFORMANCE 
 pre = Precision()
 re = Recall()
 acc = BinaryAccuracy()
@@ -83,54 +86,59 @@ for batch in test.as_numpy_iterator():
     acc.update_state(y, yhat)
 print(f'Precision:{pre.result().numpy()}, Recall:{re.result().numpy()}, Accuracy:{acc.result().numpy()}')
 
-#TEST real time
-cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
-#classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
-offset=20
-imgSize = 400
+# # #TEST real time
+# cap = cv2.VideoCapture(0)
+# detector = HandDetector(maxHands=1)
+# #classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
+# offset=20
+# imgSize = 400
+# folder = 'data123/3'
+# counter = 0
+# while True:
+#     success, img = cap.read()
+#     imgOutput = img.copy()
+#     hands, img = detector.findHands(img)
 
-while True:
-    success, img = cap.read()
-    imgOutput = img.copy()
-    hands, img = detector.findHands(img)
+#     if hands:
+#         hand = hands[0]
+#         x, y, w, h = hand['bbox']
 
-    if hands:
-        hand = hands[0]
-        x, y, w, h = hand['bbox']
+#         #devo fare in modo che le immagini crop siano tutte della stessa dimensione
+#         #e fare in modo che la mano venga inserita al centro della nostra immagine
+#         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+#         imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
 
-        #devo fare in modo che le immagini crop siano tutte della stessa dimensione
-        #e fare in modo che la mano venga inserita al centro della nostra immagine
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
-        imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
+#         aspectRatio = h/w
+#         if aspectRatio > 1:
+#             k = imgSize/h
+#             #qualsiasi valore con la virgola viene approssimato all'intero superiore
+#             wCal = math.ceil(k*w)
+#             imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+#             #devo trovare lo spazio per poter spostare l'immagine al centro
+#             wGap = math.ceil((imgSize-wCal)/2)
+#             imgWhite[ : , wGap:wCal+wGap] = imgResize
+#             # prediction, index = classifier.getPrediction(imgWhite)
+#             # print(prediction, index)
 
-        aspectRatio = h/w
-        if aspectRatio > 1:
-            k = imgSize/h
-            #qualsiasi valore con la virgola viene approssimato all'intero superiore
-            wCal = math.ceil(k*w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            #devo trovare lo spazio per poter spostare l'immagine al centro
-            wGap = math.ceil((imgSize-wCal)/2)
-            imgWhite[ : , wGap:wCal+wGap] = imgResize
-            # prediction, index = classifier.getPrediction(imgWhite)
-            # print(prediction, index)
+#         else:
+#             k = imgSize/h
+#             hCal = math.ceil(k*h)
+#             imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+#             hGap = math.ceil((imgSize-hCal)/2)
+#             imgWhite[hGap:hCal+hGap, : ] = imgResize
+#             # prediction, index = classifier.getPrediction(imgWhite)
+#             # print(prediction, index)
 
-        else:
-            k = imgSize/h
-            hCal = math.ceil(k*h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-            hGap = math.ceil((imgSize-hCal)/2)
-            imgWhite[hGap:hCal+hGap, : ] = imgResize
-            # prediction, index = classifier.getPrediction(imgWhite)
-            # print(prediction, index)
-
-        cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite", imgWhite)
+#         cv2.imshow("ImageCrop", imgCrop)
+#         cv2.imshow("ImageWhite", imgWhite)
     
-    #testo la mia immagine finale
-    resize = tf.image.resize(imgWhite, (400,400))
-    yhat = model.predict(np.expand_dims(resize/255, 0))
-    cv2.putText(imgOutput, yhat, (x, y-offset), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0 ,255), 2)
-    cv2.imshow("image", imgOutput)
-    cv2.waitKey(1)
+#     #testo la mia immagine finale
+#     #resize = tf.image.resize(imgWhite, (400,400))
+#     #yhat = model.predict(np.expand_dims(resize/255, 0))
+#     #cv2.putText(imgOutput, yhat, (x, y-offset), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0 ,255), 2)
+#     # cv2.imshow("image", imgOutput)
+#     # key = cv2.waitKey(1)
+#     # if key == ord("s"):
+#     #     counter += 1
+#     #     cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+#     #     print(counter)
